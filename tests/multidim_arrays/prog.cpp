dd9c58f1d8a67d70942eeab25822d6fe324ea3df
John McFarland <mcfarljm@gmail.com>
2012-11-18 21:48:51 +0000

    Forgot to add test and svn:ignore props with previous commit.

---
+++ tests/multidim_arrays/prog.cpp
@@ -0,0 +1,53 @@
+#include "FortWrap.h"
+#include <algorithm> 		// For count
+#include <iostream>
+
+int main(void)
+{
+  // 1-D array tests:
+  {
+    int a[3], b[3];
+    for (int i=0; i<3; i++) 
+      {
+	a[i] = i;
+	b[i] = i+1;
+      }
+    if (FortFuncs::inner_prod(3,a,b) != 8)
+      return 1;
+
+    // Test assumed size
+    if (FortFuncs::inner_prod_2(3,a,b) != 8)
+      return 1;
+  }
+
+  // 2-D array test:
+  {  
+    // Create X = [ 0, 1
+    //              1, 2
+    //              2, 3 ]
+    int X[2][3];
+    for (int i=0; i<3; i++) 
+      {
+	X[0][i] = i;
+	X[1][i] = i+1;
+      }  
+  
+    int a[2], Xa[3], Xa_test[3];
+    a[0] = 1;
+    a[1] = 2;
+    // Xa_test holds the correct solution
+    Xa_test[0] = 2;
+    Xa_test[1] = 5;
+    Xa_test[2] = 8;
+
+    FortFuncs::mat_vec_mult(3,2,(int*)X,a,Xa);
+    for (int i=0; i<3; i++) 
+      {
+	if (Xa[i] != Xa_test[i])
+	  return 3;
+      }
+  }
+
+  return 0;
+}
+
