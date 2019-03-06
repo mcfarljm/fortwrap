dd5db1424c46d874f8e82ea108de1f3d0acba5c4
John McFarland <mcfarljm@gmail.com>
2019-03-05 19:38:10 -0600

    Initial layout for testing with pointer wrapping
    Intended for wrapping function results that are pointers to derived types.

---
+++ tests/pointers/pointers.f90
@@ -0,0 +1,38 @@
+MODULE pointers
+
+  IMPLICIT NONE
+
+  TYPE Object
+    INTEGER :: x
+  END TYPE Object
+
+CONTAINS
+
+  SUBROUTINE object_ctor(o,x)
+    TYPE(Object) :: o
+    INTEGER, INTENT(in) :: x
+    o%x = x
+  END SUBROUTINE object_ctor
+
+  SUBROUTINE set_value(o,x)
+    TYPE (Object) :: o
+    INTEGER, INTENT(in) :: x
+    o%x = x
+  END SUBROUTINE set_value
+
+  FUNCTION get_value(o) RESULT(x)
+    TYPE (Object), INTENT(in) :: o
+    INTEGER :: x
+    x = o%x
+  END FUNCTION get_value
+
+  ! Todo: idea here would be to wrap this as a factory function that
+  ! returns a new Object, but internally sets up the memory automatically.
+  FUNCTION get_alias(o) RESULT(alias)
+    TYPE (Object), INTENT(in), TARGET :: o
+    TYPE (Object), POINTER :: alias
+
+    alias => o
+  END FUNCTION get_alias
+
+END MODULE pointers
---
+++ tests/pointers/prog.cpp
@@ -0,0 +1,12 @@
+#include "FortWrap.h"
+
+int main(void)
+{
+
+  Object o(1);
+
+  // Once the alias is wrapped, test it by getting an alias, modifying the original, and then verifying that the modification affected the alias.
+
+  return 0;
+}
+
