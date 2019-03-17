b34dc2aab63d80dbecd8b52f19b65cd6538b770f
John McFarland <mcfarljm@gmail.com>
2019-03-17 14:57:40 -0500

    Reworking class_pointers test
    Starting with just pointer returns of TYPE.  Next will add pointer
    returns of CLASS for concrete type.  From closer review, appears that
    for CLASS POINTER return, gfortran returns the class container by
    value, which explains why previous wrapping code was still
    working (was picking up the data pointer and then manually setting the
    vtab pointer).

--- tests/class_pointers/class_pointers.f90
+++ tests/class_pointers/class_pointers.f90
@@ -5,69 +5,45 @@ MODULE class_pointers
   TYPE, ABSTRACT :: Base
     INTEGER :: x
   CONTAINS
-    PROCEDURE :: getx
+    PROCEDURE(getval_template), DEFERRED :: getval
 !!$    PROCEDURE :: get_base_alias
-    PROCEDURE(gety_template), DEFERRED :: gety
   END TYPE Base
 
-  TYPE, EXTENDS(Base) :: Derived
-    INTEGER :: y
+  TYPE, EXTENDS(Base) :: ChildAType
   CONTAINS
-    PROCEDURE :: gety
-    PROCEDURE :: get_t_alias => get_derived_t_alias_class
-    PROCEDURE :: get_c_alias => get_derived_c_alias
-  END TYPE Derived
+    PROCEDURE :: getval => childa_getval
+    PROCEDURE :: alias_t => childa_alias_t
+  END TYPE ChildAType
 
   ABSTRACT INTERFACE
-    FUNCTION gety_template(b) RESULT(y)
+    FUNCTION getval_template(self) RESULT(val)
       IMPORT Base
-      CLASS (Base), INTENT(in) :: b
-      INTEGER :: y
-    END FUNCTION gety_template
+      CLASS (Base) :: self
+      INTEGER :: val
+    END FUNCTION getval_template
   END INTERFACE
 
 CONTAINS
 
-  SUBROUTINE derived_ctor(d, x, y)
-    CLASS (Derived) :: d
-    INTEGER, INTENT(in) :: x, y
-    d%x = x
-    d%y = y
-  END SUBROUTINE derived_ctor
+  SUBROUTINE childa_ctor(c, x)
+    CLASS (ChildAType) :: c
+    INTEGER, INTENT(in) :: x
+    c%x = x
+  END SUBROUTINE childa_ctor
 
-  FUNCTION getx(b)
-    CLASS (Base), INTENT(in) :: b
-    INTEGER :: getx
-    getx = b%x
-  END FUNCTION getx
+  FUNCTION childa_getval(self) RESULT(val)
+    CLASS (ChildAType) :: self
+    INTEGER :: val
+    val = self%x
+  END FUNCTION childa_getval
 
-  FUNCTION gety(b) RESULT(y)
-    CLASS (Derived), INTENT(in) :: b
-    INTEGER :: y
-    y = b%y
-  END FUNCTION gety
+  FUNCTION childa_alias_t(self) RESULT(p)
+    CLASS (ChildAType), TARGET :: self
+    TYPE (ChildAType), POINTER :: p
+    p => self
+  END FUNCTION childa_alias_t
 
-  FUNCTION get_derived_t_alias(d) RESULT(p)
-    TYPE (Derived), INTENT(in), TARGET :: d
-    TYPE (Derived), POINTER :: p
-
-    p => d
-  END FUNCTION get_derived_t_alias
-
-  FUNCTION get_derived_t_alias_class(d) RESULT(p)
-    CLASS (Derived), INTENT(in), TARGET :: d
-    TYPE (Derived), POINTER :: p
-
-    p => d
-  END FUNCTION get_derived_t_alias_class
-
-  FUNCTION get_derived_c_alias(d) RESULT(p)
-    CLASS (Derived), INTENT(in), TARGET :: d
-    CLASS (Derived), POINTER :: p
-
-    p => d
-  END FUNCTION get_derived_c_alias
-
+  
 !!$  FUNCTION get_base_alias(b) RESULT(p)
 !!$    CLASS (Base), INTENT(in), TARGET :: b
 !!$    CLASS (Base), POINTER :: p
--- tests/class_pointers/prog.cpp
+++ tests/class_pointers/prog.cpp
@@ -4,33 +4,15 @@
 int main(void)
 {
 
-  Base *b = new Derived(1,2);
+  ChildAType* childa = new ChildAType(1);
 
-  printf("getx: %d\n", b->getx());
-  printf("gety: %d\n", b->gety());
+  if (childa->getval() != 1)
+    return 1;
 
-  Derived *d = new Derived(3,4);
-
-  Derived *dp = d->get_derived_t_alias();
-
-  printf("getx: %d\n", dp->getx());
-  printf("gety: %d\n", dp->gety());
-
-  Derived *dp2 = d->get_t_alias();
-
-  printf("getx: %d\n", dp2->getx());
-  printf("gety: %d\n", dp2->gety());
-
-  Derived *dp3 = d->get_c_alias();
-
-  printf("getx: %d\n", dp3->getx());
-  printf("gety: %d\n", dp3->gety());  
-
-  delete b;
-  delete d;
-  delete dp;
-  delete dp2;
-  delete dp3;
+  ChildAType* pa = childa->alias_t();
+  if (pa->getval() != 1)
+    return 1;
+  
 
   return 0;
 }
