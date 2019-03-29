a39ea68cb2f807a57cac2993aa5b3d3b67c7a9d6
John McFarland <mcfarljm@gmail.com>
2019-03-28 20:32:53 -0500

    Adding test cases with logical function arguments
    Previously only return values were tested

--- tests/logicals/logicals.f90
+++ tests/logicals/logicals.f90
@@ -25,4 +25,24 @@ CONTAINS
     get_iso_false = .FALSE.
   END FUNCTION get_iso_false
 
+  FUNCTION logical_in(l)
+    LOGICAL, INTENT(in) :: l
+    INTEGER :: logical_in
+    IF (l) THEN
+      logical_in = 1
+    ELSE
+      logical_in = 0
+    END IF
+  END FUNCTION logical_in
+
+  SUBROUTINE true_out(l)
+    LOGICAL, INTENT(out) :: l
+    l = .TRUE.
+  END SUBROUTINE true_out
+
+  SUBROUTINE false_out(l)
+    LOGICAL, INTENT(out) :: l
+    l = .FALSE.
+  END SUBROUTINE false_out
+
 END MODULE logicals
--- tests/logicals/prog.cpp
+++ tests/logicals/prog.cpp
@@ -11,7 +11,17 @@ int main(void)
   if (! FortFuncs::get_iso_true())
     return 11;
   if (FortFuncs::get_iso_false())
-    return 12;  
+    return 12;
+
+  if (FortFuncs::logical_in(1) != 1)
+    return 20;
+  bool l;
+  FortFuncs::true_out(&l);
+  if (!l)
+    return 30;
+  FortFuncs::false_out(&l);
+  if (l)
+    return 31;
 
   return 0;
 }
