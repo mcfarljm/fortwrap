1d5156786e07dc927b9b864e6a4cef96f860e5c3
John McFarland <mcfarljm@gmail.com>
2019-04-16 07:50:44 -0500

    Add %init keyword to configuration file
    The previously used "initialized" member is no longer added to proxy
    classes by default.  Now it can be added using the %init keyword in
    the configuration file.  The keyword specifies a regular expression
    for initialization functions that must be called before the destructor
    is called.
    
    Updating manual to document %init

--- fortwrap.py
+++ fortwrap.py
@@ -117,6 +117,8 @@ enum_def = re.compile(r'\s*ENUM,\s*BIND',re.IGNORECASE)
 # Constructor and desctructor methods (these regexes are configurable):
 ctor_def = re.compile('.*_ctor', re.IGNORECASE)
 dtor_def = re.compile('.*_dtor', re.IGNORECASE)
+# Regular expression for "initialization" functions (configurable):
+init_func_def = None
 
 # ===================================================
 
@@ -1828,9 +1830,11 @@ def write_constructor(file,object,fort_ctor=None):
     if fort_ctor:
         file.write(function_def_str(fort_ctor,bind=True,call=True) )
         file.write(' // Fortran Constructor\n')
-        file.write('  initialized = true;\n')
+        if init_func_def:
+            file.write('  initialized = true;\n')
     else:
-        file.write('  initialized = false;\n')
+        if init_func_def:
+            file.write('  initialized = false;\n')
     file.write('}\n\n')    
 
 def write_destructor(file,object):
@@ -1843,7 +1847,8 @@ def write_destructor(file,object):
     for proc in object.procs:
         if proc.dtor:
             target = 'data_ptr'
-            file.write('  ' + 'if (initialized) ' + proc.c_binding_name() + '(' + target)
+            prefix = 'if (initialized) ' if init_func_def else ''
+            file.write('  ' + prefix + proc.c_binding_name() + '(' + target)
             # Add NULL for any optional arguments (only optional
             # arguments are allowed in the destructor call)
             for i in range(proc.nargs-1):
@@ -1950,8 +1955,9 @@ def write_class(object):
     #file.write('\nprivate:\n')
     if object.name!=orphan_classname and not object.extends:
         file.write('  ADDRESS data_ptr;\n')
-        file.write('\nprotected:\n')
-        file.write('  bool initialized;\n')
+        if init_func_def:
+            file.write('\nprotected:\n')
+            file.write('  bool initialized;\n')
     if not opts.global_orphans:
         file.write('};\n\n')
     if object.name == orphan_classname:
@@ -1993,12 +1999,12 @@ def write_class(object):
         if proc.ctor or (proc.dtor and not proc.name.lower() in name_inclusions):
             continue
         file.write(function_def_str(proc,obj=object,prefix='') + ' {\n')
-        if proc.dtor:
+        if proc.dtor and init_func_def:
             file.write('  if (initialized) ')
         file.write(function_def_str(proc,bind=True,call=True,prefix='  ') + '\n')
-        if proc.dtor:
+        if proc.dtor and init_func_def:
             file.write('  initialized = false;\n')
-        elif proc.name.lower().startswith('new_'):
+        elif init_func_def and init_func_def.match(proc.name):
             file.write('  initialized = true;\n')
         file.write('}\n\n')
 
@@ -2309,7 +2315,7 @@ class ConfigurationFile(object):
             self.process()
 
     def process(self):
-        global name_exclusions, name_inclusions, name_substitutions, ctor_def, dtor_def
+        global name_exclusions, name_inclusions, name_substitutions, ctor_def, dtor_def, init_func_def
         for line_num,line in enumerate(self.f):
             if not line.startswith('%'):
                 continue
@@ -2356,6 +2362,8 @@ class ConfigurationFile(object):
                 ctor_def = re.compile(line.strip().split('%ctor ')[1], re.IGNORECASE)
             elif words[0] == '%dtor':
                 dtor_def = re.compile(line.strip().split('%dtor ')[1], re.IGNORECASE)
+            elif words[0] == '%init':
+                init_func_def = re.compile(line.strip().split('%init ')[1], re.IGNORECASE)
             else:
                 error("Unrecognized declaration in interface file: {}".format(words[0]))
                 
